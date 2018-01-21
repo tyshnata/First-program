8f83fc248998eb032ced424c190b96776ee3050b
tyshnata <tyshnata@gmail.com>
2018-01-21 22:28:31 +0200

    добавился метод, возвращающий n-й элемент последовательности Фибоначчи

--- Maximum/src/Maximum.java
+++ Maximum/src/Maximum.java
@@ -2,17 +2,18 @@ import java.util.List;
 
 public class Maximum {
 
-    public  Integer maximumNum (Integer[] arrlist){ //метод поиска наибольшего числа в списке
-        Integer maxNumber = arrlist[0];
+    public  Integer maximumNum (Integer[] list){ //метод поиска наибольшего числа в списке
+        Integer maxNumber = list[0];
 
-        for(int i = 0; i < arrlist.length ; i++){ // перебираем элементы массива, ищем Мах
-            if ( maxNumber >= arrlist[i] ){}
-            else maxNumber = arrlist[i];
+        for(int i = 0; i < list.length ; i++){ // перебираем элементы массива, ищем Мах
+            if ( maxNumber >= list[i] ){}
+            else maxNumber = list[i];
         }
         return maxNumber; // возвращаем Мах элемент
     }
 
-    public static Integer getElement(List<Integer> list, Integer n){ //метод, возвращающий n-й элемент списка
+
+    public Integer getElement(List<Integer> list, Integer n){ //метод, возвращающий n-й элемент списка
         Integer number;
         if ( n < list.size()){  // если индекс не превышает размера списка
             number = list.get(n); // получаем значение элемента по индексу
@@ -24,7 +25,30 @@ public class Maximum {
     }
 
 
+   public static Integer fibonacci(Integer n){ // метод, воввращающий n-й элемент последовательности Фибоначчи
+        Integer fibElement = 0;
+        Integer i1 = 0, i2 = 1;
+        if (n == 0) {
+            fibElement = 0; // элемент посл-ти Фибоначчи с индексом "0"
+        }
+        else {
+            if (n == 1) {
+                fibElement = 1; //элемент посл-ти Фибоначчи с индексом "1"
+            }
+            else {
+                for (int i = 2; i <= n; i++) {
+                    fibElement = i1 + i2;
+                    i1 = i2;
+                    i2 =  fibElement;
 
+                }
+            }
+        }
+
+        return fibElement; //элемент посл-ти Фибоначчи с индексом "n"
+   }
+
+}
 
 
 
